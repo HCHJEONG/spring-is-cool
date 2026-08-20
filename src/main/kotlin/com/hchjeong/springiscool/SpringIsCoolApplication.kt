3a383693066087470b747736bbbd2236cc08cf0d
hchjeong <hcjeong@kakao.com>
2026-08-20 11:27:26 +0900

    Add Kotlin Spring Boot skeleton

---
+++ src/main/kotlin/com/hchjeong/springiscool/SpringIsCoolApplication.kt
@@ -0,0 +1,11 @@
+package com.hchjeong.springiscool
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringIsCoolApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringIsCoolApplication>(*args)
+}
