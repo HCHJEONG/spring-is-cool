f3b2d2384a65d31f9b9fd34158579d1f994b07f0
hchjeong <hcjeong@kakao.com>
2026-08-20 11:44:55 +0900

    Add Apache MINA SSHD dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.apache.sshd:sshd-core:2.14.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
